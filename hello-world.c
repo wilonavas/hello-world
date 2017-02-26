7e549ca932623dc6a0b7089758b40c10d6c872fa
wjna <wilo@wilos-MacBook-Air.local>
2017-02-26 17:48:33 -0400

    Second commit without push

--- hello-world.c
+++ hello-world.c
@@ -2,7 +2,5 @@
 
 int main(int argc, char* argv[]){
   printf("Hello World!\n");
-  printf("Making a local change here.\n");
-  printf("Making a local change there.\n");
   printf("Try a change directly on the master.\n");
 }
