3f5cfed2c1b5ebadc7d8238ea50fe4535272cfee
wjna <wilo@wilos-MacBook-Air.local>
2017-02-26 20:33:35 -0400

    Revert "Second commit without push"
    This reverts commit 7e549ca932623dc6a0b7089758b40c10d6c872fa.

--- hello-world.c
+++ hello-world.c
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
   printf("Hello World!\n");
   printf("Try a change directly on the master.\n");
 }
+=======
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+  printf("Hello World!\n");
+  printf("Making a local change here.\n");
+  printf("Making a local change there.\n");
+  printf("Try a change directly on the master.\n");
+}
+>>>>>>> parent of 7e549ca... Second commit without push
