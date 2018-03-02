ef7d1d3ae7e7c48a92d3549a6d04d0223fb0278d
U-AMERICAS\WJN <WJN@WJN8.americas.hpqcorp.net>
2018-03-02 11:09:12 -0400

    Second commit from cygwin git outside the hpe proxy

--- hello-world.c
+++ hello-world.c
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-int main(int argc, char* argv[]){
-  printf("Hello World!\n");
-  printf("Try a change directly on the master.\n");
-}
-=======
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
@@ -14,4 +6,3 @@ int main(int argc, char* argv[]){
   printf("Making a local change there.\n");
   printf("Try a change directly on the master.\n");
 }
->>>>>>> parent of 7e549ca... Second commit without push
